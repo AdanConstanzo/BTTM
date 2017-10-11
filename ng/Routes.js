2a52aab8a8e2ff74722fcb997d6b2df52eca7fe2
Adan <aconstanzolan@gmail.com>
2017-10-11 10:08:48 -0700

    Re-routes when server status hits a 401 to login page. Also re-routes to homepage when anything not specififed in routes. Plus commented out the nav item.

--- ng/Routes.js
+++ ng/Routes.js
@@ -1,8 +1,26 @@
 angular.module("app")
-    .config(function ($routeProvider) {
-        $routeProvider
-            .when("/",{templateUrl:"/templates/Home.html"})
-            .when('/register',{controller:'RegisterCtrl',templateUrl:'/templates/Register.html'})
-            .when('/login',{controller:'LoginCtrl',templateUrl:'/templates/Login.html'})
-            .when('/barter-user-:otheruser',{controller:'BarterCtrl',templateUrl:'/templates/Barter.html'})
-    });
+  .config(function ($routeProvider) {
+      $routeProvider
+          .when("/",{templateUrl:"/templates/Home.html"})
+          .when('/register',{controller:'RegisterCtrl',templateUrl:'/templates/Register.html'})
+          .when('/login',{controller:'LoginCtrl',templateUrl:'/templates/Login.html'})
+          .when('/barter-user-:otheruser',{controller:'BarterCtrl',templateUrl:'/templates/Barter.html'})
+          .otherwise({redirectTo: '/'});
+  })
+  .factory('authHttpResponseInterceptor',['$q','$location',function($q,$location){
+    return {
+    	response: function(response){
+    		return response || $q.when(response);
+    	},
+    	responseError: function(rejection) {
+    		if (rejection.status === 401) {
+    			$location.path('/login').search('returnTo', $location.path());
+    		}
+    		return $q.reject(rejection);
+    	}
+    }
+  }])
+  .config(['$httpProvider',function($httpProvider) {
+    //Http Intercpetor to check auth failures for xhr requests
+    $httpProvider.interceptors.push('authHttpResponseInterceptor');
+  }]);
