3f89f520b63520549ca9161e1d361aef3b9e5e11
Cafemanager <kevinflores1997@yahoo.com>
2017-10-27 11:54:49 -0700

    Fix trading objects date and created trading api

--- models/TradingObject.js
+++ models/TradingObject.js
@@ -5,9 +5,9 @@ var schema = new Schema({
     name :          {type:String,required:true},
     description :   {type:String,required:true},
     image :         [String],
-    dateOf:         {type:Date,required:true},
-    user  :         {type:String,required:true, default: Date.now}
-    
+    dateOf:         {type:Date,required:true,default: Date.now},
+    user  :         {type:String,required:true}
+
 });
 
 module.exports = mongoose.model('TradingObject',schema);
