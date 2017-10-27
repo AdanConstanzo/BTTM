348969d7e5ab341cea8adba3af199811be2aeb2c
Cafemanager <kevinflores1997@yahoo.com>
2017-10-27 11:31:24 -0700

    updated TradingObects.js

--- models/TradingObject.js
+++ models/TradingObject.js
@@ -4,9 +4,10 @@ var Schema = mongoose.Schema;
 var schema = new Schema({
     name :          {type:String,required:true},
     description :   {type:String,required:true},
-    image :         {type:String,required:true},
+    image :         [String],
     dateOf:         {type:Date,required:true},
-    user  :         {type:String,required:true}
+    user  :         {type:String,required:true, default: Date.now}
+    
 });
 
 module.exports = mongoose.model('TradingObject',schema);
