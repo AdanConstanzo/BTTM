729e4334812731f8755a8ee6baa6e4a8ac77c6f7
Iovanni Enriquez <iovanni.enriquez@aol.com>
2017-10-28 16:26:38 -0700

    Added google maps prototype

--- ng/Routes.js
+++ ng/Routes.js
@@ -4,4 +4,5 @@ angular.module("app")
             .when("/",{templateUrl:"/templates/Home.html"})
             .when('/register',{controller:'RegisterCtrl',templateUrl:'/templates/Register.html'})
             .when('/login',{controller:'LoginCtrl',templateUrl:'/templates/Login.html'})
+            .when('/user',{controller:'UserCtrl', templateUrl:'/templates/User.html'})
     });
