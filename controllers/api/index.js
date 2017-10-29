6c263b67dd0219c2a464006e6b2bc941d4920a32
Cafemanager <kevinflores1997@yahoo.com>
2017-10-28 17:40:45 -0700

    filler

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,9 +5,6 @@ var router = require("express").Router();
 // links our routers to the server.js
 router.use(require("./users"));
 router.use(require("./sessions"));
-<<<<<<< Updated upstream
-
-=======
 router.use(require("./trading"));
 router.use(require("./chat"));
 >>>>>>> Stashed changes
