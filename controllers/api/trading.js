d9a928859431fa2248a61bb4f651abfaa0932c4c
Cafemanager <kevinflores1997@yahoo.com>
2017-11-02 20:04:30 -0700

    added functions for trading items

--- controllers/api/trading.js
+++ controllers/api/trading.js
@@ -5,20 +5,23 @@ var Trade  = require('../../models/TradingObject');
 var config = require('../../config');
 var multer = require('multer');
 
+//creates a new item
+
 router.post('/trading/addItem',function(req,res, next){
   var tempTrade = new Trade({
     name:req.body.name,
     description:req.body.description,
     image:["apple","pen"],
     user:req.body.user
-
+  });
   tempTrade.save(function(err,prog){
     if(err){return next(err)}
     res.json({status:"success",code:201});
-  })
+  });
 });
-})
 
+
+// returns all items of the user
 router.get("/trading/getItems/:username",function(req,res,next){
   var username = req.params.username;
   Trade.find({"user":username})
@@ -33,11 +36,47 @@ router.get("/trading/getItems/:username",function(req,res,next){
   })
 })
 
-router.delete("/trading/deleteItems/",function(req,res, next){
-
+router.get("/trading/getItems/findOne/:id",function(req,res,next){
+  var id = req.params.id;
+  Trade.findOne({_id: id})
+  .exec(function(err,user){
+    if(err) {
+      res.json({"error": "oh no."})
+      return;
+    }
+    if(!user){
+      res.json({"message":"no user"})
+      return;
+    }
+    res.send(user);
+  })
 })
+
+// deletes an item by id
+router.delete("/trading/deleteItems/:id",function(req,res, next){
+  var id = req.params.id;
+  Trade.remove({_id: id},function(err,result){
+    if(err){
+      return res.json({error:"Missing sometig"})
+    }
+    res.sendStatus(200);
+  });
+});
+
+// edits an item based on id
+router.put("/trading/editItems/:id",function(req,res, next){
+  var id = req.params.id;
+  Trade.findByIdAndUpdate({_id : id},{
+      name:req.body.name,
+      description:req.body.description,
+      image:["apple","pen"],
+    },function(err,docs){
+      if(err){return next(err)}
+      res.sendStatus(200);
+    });
+});
 /*
-need .delete method to delete items
+
 need .put method to edit items
 */
 /*  User.findOne({"username":username})
