23dba62e57c2f6e4b29cc625f9f74b1d73d26f7b
Adan Constanzo <aconstanzolan@gmail.com>
2017-11-10 23:45:01 -0800

    Added my items page.

---
+++ ng/ctrl.my_items.js
@@ -0,0 +1,27 @@
+angular.module("app").controller("MyItemsCtrl",function($scope,UserSvc,TradingItemSvc){
+
+    UserSvc.getUserAccountInfo()
+        .then( function(UserObject) {
+            $scope.Users = UserObject;
+            TradingItemSvc.getAllUserTradingItems(UserObject.username)
+                .then(function (TradingObject) {
+                    for(x in TradingObject) {
+                        if (TradingObject[x].description.length>55) {
+                            TradingObject[x].description = TradingObject[x].description.substring(0, 55) + "...";
+                        } else {
+                            TradingObject[x].description = FillString(TradingObject[x].description,55-TradingObject[x].description.length);
+                        }
+                    }
+                    $scope.UserItems = TradingObject;
+                });
+        });
+
+
+    function FillString(string,numberOf) {
+        for (var i = 0; i < numberOf; i++) {
+            string += "\xa0"
+        }
+        return string;
+    }
+
+});
