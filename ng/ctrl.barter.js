9dadb26d9986542316e1e704b5240be7f61c096d
Iovanni Enriquez <iovanni.enriquez@aol.com>
2017-11-11 20:56:17 -0800

    updated drag/drop in barter page

--- ng/ctrl.barter.js
+++ ng/ctrl.barter.js
@@ -5,27 +5,25 @@ angular.module("app").controller("BarterCtrl", function ($scope, $routeParams, C
   barterController.user = "";
   $scope.otherUser = barterController.otherUser;
 
+  
   $(function () {
 
-    // There's the gallery and the pool
+    // There's the gallery and the trash
     var $gallery = $("#gallery"),
-      $pool = $("#pool");
+      $trash = $("#trash");
 
     // Let the gallery items be draggable
     $("li", $gallery).draggable({
+
       cancel: "a.ui-icon", // clicking an icon won't initiate dragging
       revert: "invalid", // when not dropped, the item will revert back to its initial position
-      containment: "document",
+      containment: "#containment-wrapper",
       helper: "clone",
       cursor: "move"
     });
 
-    $(function () {
-      $("#draggable1").draggable({ containment: "#useritems, scroll" })
-    });
-
-    // Let the pool be droppable, accepting the gallery items
-    $pool.droppable({
+    // Let the trash be droppable, accepting the gallery items
+    $trash.droppable({
       accept: "#gallery > li",
       classes: {
         "ui-droppable-active": "ui-state-highlight"
@@ -35,9 +33,9 @@ angular.module("app").controller("BarterCtrl", function ($scope, $routeParams, C
       }
     });
 
-    // Let the gallery be droppable as well, accepting items from the pool
+    // Let the gallery be droppable as well, accepting items from the trash
     $gallery.droppable({
-      accept: "#pool li",
+      accept: "#trash li",
       classes: {
         "ui-droppable-active": "custom-state-active"
       },
@@ -50,9 +48,9 @@ angular.module("app").controller("BarterCtrl", function ($scope, $routeParams, C
     var recycle_icon = "<a href='link/to/recycle/script/when/we/have/js/off' title='Remove this image' class='ui-icon ui-icon-refresh'>Remove Item</a>";
     function deleteImage($item) {
       $item.fadeOut(function () {
-        var $list = $("ul", $pool).length ?
-          $("ul", $pool) :
-          $("<ul class='gallery ui-helper-reset'/>").appendTo($pool);
+        var $list = $("ul", $trash).length ?
+          $("ul", $trash) :
+          $("<ul class='gallery ui-helper-reset'/>").appendTo($trash);
 
         $item.find("a.ui-icon-trash").remove();
         $item.append(recycle_icon).appendTo($list).fadeIn(function () {
@@ -65,7 +63,7 @@ angular.module("app").controller("BarterCtrl", function ($scope, $routeParams, C
     }
 
     // Image recycle function
-    var pool_icon = "<a href='link/to/trash/script/when/we/have/js/off' title='Remove this image' class='ui-icon ui-icon-trash'>Delete image</a>";
+    var trash_icon = "<a href='link/to/trash/script/when/we/have/js/off' title='Remove this image' class='ui-icon ui-icon-trash'>Delete image</a>";
     function recycleImage($item) {
       $item.fadeOut(function () {
         $item
@@ -73,7 +71,7 @@ angular.module("app").controller("BarterCtrl", function ($scope, $routeParams, C
           .remove()
           .end()
           .css("width", "96px")
-          .append(pool_icon)
+          .append(trash_icon)
           .find("img")
           .css("height", "72px")
           .end()
@@ -119,6 +117,119 @@ angular.module("app").controller("BarterCtrl", function ($scope, $routeParams, C
       return false;
     });
   });
+
+  //for the other user
+  $(function () {
+    
+        // There's the gallery2 and the trash2
+        var $gallery2 = $("#gallery2"),
+          $trash2 = $("#trash2");
+    
+        // Let the gallery2 items be draggable
+        $("li", $gallery2).draggable({
+    
+          cancel: "a.ui-icon", // clicking an icon won't initiate dragging
+          revert: "invalid", // when not dropped, the item will revert back to its initial position
+          containment: "#containment-wrapper2",
+          helper: "clone",
+          cursor: "move"
+        });
+    
+        // Let the trash2 be droppable, accepting the gallery2 items
+        $trash2.droppable({
+          accept: "#gallery2 > li",
+          classes: {
+            "ui-droppable-active": "ui-state-highlight"
+          },
+          drop: function (event, ui) {
+            deleteImage2(ui.draggable);
+          }
+        });
+    
+        // Let the gallery2 be droppable as well, accepting items from the trash2
+        $gallery2.droppable({
+          accept: "#trash2 li",
+          classes: {
+            "ui-droppable-active": "custom-state-active"
+          },
+          drop: function (event, ui) {
+            recycleImage2(ui.draggable);
+          }
+        });
+    
+        // Image deletion function
+        var recycle_icon2 = "<a href='link/to/recycle2/script/when/we/have/js/off' title='Remove this image' class='ui-icon ui-icon-refresh'>Remove Item</a>";
+        function deleteImage2($item2) {
+          $item2.fadeOut(function () {
+            var $list2 = $("ul", $trash2).length ?
+              $("ul", $trash2) :
+              $("<ul class='gallery2 ui-helper-reset'/>").appendTo($trash2);
+    
+            $item2.find("a.ui-icon-trash").remove();
+            $item2.append(recycle_icon2).appendTo($list2).fadeIn(function () {
+              $item2
+                .animate({ width: "48px" })
+                .find("img")
+                .animate({ height: "36px" });
+            });
+          });
+        }
+    
+        // Image recycle function
+        var trash_icon2 = "<a href='link/to/trash2/script/when/we/have/js/off' title='Remove this image' class='ui-icon ui-icon-trash'>Delete image</a>";
+        function recycleImage2($item2) {
+          $item2.fadeOut(function () {
+            $item2
+              .find("a.ui-icon-refresh")
+              .remove()
+              .end()
+              .css("width", "96px")
+              .append(trash_icon2)
+              .find("img")
+              .css("height", "72px")
+              .end()
+              .appendTo($gallery2)
+              .fadeIn();
+          });
+        }
+    
+        // Image preview function, demonstrating the ui.dialog used as a modal window
+        function viewLargerImage2($link2) {
+          var src2 = $link2.attr("href"),
+            title2 = $link2.siblings("img").attr("alt"),
+            $modal2 = $("img[src$='" + src2 + "']");
+    
+          if ($modal2.length) {
+            $modal2.dialog("open");
+          } else {
+            var img2 = $("<img alt='" + title + "' width='384' height='288' style='display: none; padding: 8px;' />")
+              .attr("src", src2).appendTo("body");
+            setTimeout(function () {
+              img2.dialog({
+                title: title,
+                width: 400,
+                modal: true
+              });
+            }, 1);
+          }
+        }
+    
+        // Resolve the icons behavior with event delegation
+        $("ul.gallery2 > li").on("click", function (event) {
+          var $item2 = $(this),
+            $target2 = $(event.target);
+    
+          if ($target2.is("a.ui-icon-trash")) {
+            deleteImage($item2);
+          } else if ($target2.is("a.ui-icon-zoomin")) {
+            viewLargerImage($target2);
+          } else if ($target2.is("a.ui-icon-refresh")) {
+            recycleImage($item2);
+          }
+    
+          return false;
+        });
+      });
   //-----------------------------------------------------------------
 
   // get's current user's name.
