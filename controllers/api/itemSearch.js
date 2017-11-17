b382df6635561ca9e868f17cdd13c2965a0c7381
Cafemanager <kevinflores1997@yahoo.com>
2017-11-17 15:47:49 -0800

    added a search function

---
+++ controllers/api/itemSearch.js
@@ -0,0 +1,24 @@
+var router = require('express').Router();
+var TradeObjs = require('../../models/TradingObject');
+
+
+
+router.get("/itemSearch/search/:name",function(req,res,next){
+  var search = req.params.name;
+  var counter = 0;
+  var temp;
+  TradeObjs.find()
+    .exec(function(err,arr){
+        var sendThis = [];
+        for(x in arr){
+          temp = arr[x].name;
+          if(temp.includes(search)){
+            sendThis.push(arr[x]);
+            counter++;
+          }
+        }
+        res.send(sendThis);
+    });
+});
+
+module.exports = router;
